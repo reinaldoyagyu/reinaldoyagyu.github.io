faa7f57baffe11d920a3f44cdbd35a839aae24a0
Jikojin <60075572+reinaldoyagyu@users.noreply.github.com>
2020-04-20 20:03:22 -0300

    Clock working with alarms and sound
    added sound to its alams, changed background color

--- script.js
+++ script.js
@@ -5,30 +5,45 @@ function startTime() {
   var s = today.getSeconds()
   m = checkTime(m)
   s = checkTime(s)
-  document.querySelector(".clock").innerHTML = h + ":" + m + ":" + s
+  document.querySelector("#clock").innerHTML = h + ":" + m + ":" + s
+  if (h >= 8 && h <= 18) { var workHour = true }
+  else { workHour = false }
+  if ((m >= 55) || (m >= 25 && m <= 30)) { var interval = true }
+  else { interval = false }
+  if (workHour && !interval) {
+    document.querySelector("#description").innerHTML = "Work work!!!"
+    document.body.className = "focus"
+  }
+  if (workHour && interval) {
+    document.querySelector("#description").innerHTML = "Get up and rest!!!"
+    document.body.className = "rest"
+  }
+  if (!workHour && !interval) {
+    document.querySelector("#description").innerHTML = "Do whatever you want"
+    document.body.className = "play"
+  }
+  if (!workHour && interval) {
+    document.querySelector("#description").innerHTML = "Get up!!!"
+    document.body.className = "getUp"
+  }
+  switch (m) {
+    case 0:
+    case 5:
+    case 25:
+    case 30:
+    case 55:
+      if (document.querySelector("#withSound").checked == true && s == 0) {
+        var audio = document.querySelector("#myAudio")
+        audio.play()
+      }
+      break
+  }
   var t = setTimeout(startTime, 1000)
-  alarm(h, m, s)
 }
 
 function checkTime(i) {
   if (i < 10) { i = "0" + i }
   return i
-}
-
-function alarm(h, m, s) {
-  if ((h >= 8 && h <= 11) || (h >= 13 && h <= 18)) {
-    if ((m >= 0 && m <= 24) || (m >= 30 && m <= 54)) {
-      document.querySelector(".description").innerHTML = "work work!!!"
-    } else {
-      document.querySelector(".description").innerHTML = "Get up and rest!!!"
-    }
-  } else {
-    if ((m >= 0 && m <= 24) || (m >= 30 && m <= 54)) {
-      document.querySelector(".description").innerHTML = "Do whatever you want"
-    } else {
-      document.querySelector(".description").innerHTML = "Get up!!!"
-    }
-  }
 }
 
 startTime()
