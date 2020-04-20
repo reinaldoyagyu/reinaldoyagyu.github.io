4d9b87ff58aeb250001e258304e6049904cc353f
Jikojin <60075572+reinaldoyagyu@users.noreply.github.com>
2020-04-20 00:16:58 -0300

    Clock works!
    Clock works fine and first draft on conditions set

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Clock
  Clock with alarms
+ Just another tool to improve productivity
+ This is my first try to make a free tool and publish it
---
+++ script.js
@@ -0,0 +1,35 @@
+function startTime() {
+  var today = new Date()
+  var h = today.getHours()
+  var m = today.getMinutes()
+  var s = today.getSeconds()
+  m = checkTime(m)
+  s = checkTime(s)
+  document.querySelector(".clock").innerHTML = h + ":" + m + ":" + s
+  var t = setTimeout(startTime, 1000)
+  alarm(h, m, s)
+}
+
+function checkTime(i) {
+  if (i < 10) { i = "0" + i }
+  return i
+}
+
+function alarm(h, m, s) {
+  if ((h >= 8 && h <= 11) || (h >= 13 && h <= 18)) {
+    if ((m >= 0 && m <= 24) || (m == 30 && m <= 54)) {
+      document.querySelector(".description").innerHTML = "work work!!!"
+    } if ((m >= 25 && m <= 29) || (m == 55 && m <= 59)) {
+      document.querySelector(".description").innerHTML = "Get up and rest!!!"
+    }
+  }
+  else {
+    if ((m >= 0 && m <= 24) || (m == 30 && m <= 54)) {
+      document.querySelector(".description").innerHTML = "Do whatever you want"
+    } if ((m >= 25 && m <= 29) || (m == 55 && m <= 59)) {
+      document.querySelector(".description").innerHTML = "Get up!!!"
+    }
+  }
+}
+
+startTime()
